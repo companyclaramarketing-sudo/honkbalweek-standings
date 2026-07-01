3467a5bbbe4bb5a0caebfc6e2fb012f38d724f12
companyclaramarketing-sudo <company.claramarketing@gmail.com>
2026-07-01 11:31:37 +0200

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -9,6 +9,8 @@ const fs = require('fs');
   });
   const page = await browser.newPage();
 
+  await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36');
+
   await page.goto('https://stats.knbsbstats.nl/en/events/2026-honkbalweek-haarlem/standings', {
     waitUntil: 'domcontentloaded',
     timeout: 60000
